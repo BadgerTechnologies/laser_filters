cc8164167554884d83c66be2aadf27bb497e4060
Tyler Buchman <tyler_buchman@jabil.com>
2021-12-04 01:19:21 -0500

    Adding NodeHandle to ScanToCloudFilterChainNodelet

--- src/scan_to_cloud_filter_chain.cpp
+++ src/scan_to_cloud_filter_chain.cpp
@@ -109,8 +109,8 @@ public:
   bool  incident_angle_correction_;
 
   ////////////////////////////////////////////////////////////////////////////////
-  ScanToCloudFilterChain (ros::NodeHandle& pnh, const std::string& name) :
-      laser_max_range_ (DBL_MAX), private_nh(pnh), name_(name), filter_(tf_, "", 50),
+  ScanToCloudFilterChain (ros::NodeHandle& nh, ros::NodeHandle& pnh, const std::string& name) :
+      laser_max_range_ (DBL_MAX), nh(nh), private_nh(pnh), name_(name), filter_(tf_, "", 50),
       cloud_filter_chain_("sensor_msgs::PointCloud2"), scan_filter_chain_("sensor_msgs::LaserScan")
   {
     private_nh.param("high_fidelity", high_fidelity_, false);
@@ -287,7 +287,9 @@ class ScanToCloudFilterChainNodelet : public nodelet::Nodelet
   std::unique_ptr<ScanToCloudFilterChain> chain_;
 
   void onInit() override {
-    chain_ = std::unique_ptr<ScanToCloudFilterChain>(new ScanToCloudFilterChain(getPrivateNodeHandle(), getName()));
+    chain_ = std::unique_ptr<ScanToCloudFilterChain>(new ScanToCloudFilterChain(getNodeHandle(),
+                                                                                getPrivateNodeHandle(),
+                                                                                getName()));
   }
 };
 
@@ -297,8 +299,9 @@ int
 main (int argc, char** argv)
 {
   ros::init (argc, argv, "scan_to_cloud_filter_chain");
+  ros::NodeHandle nh("");
   ros::NodeHandle pnh("~");
-  ScanToCloudFilterChain f(pnh, "");
+  ScanToCloudFilterChain f(nh, pnh, "");
 
   ros::spin();
 
